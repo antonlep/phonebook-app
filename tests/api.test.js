5882a57721a4847eb0e02116ef9054f27ce4f1a0
antonlep <antonleppa@gmail.com>
2022-12-09 21:54:48 +0200

    configure tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,42 +1,11 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const Person = require('../models/person')
-
-const api = supertest(app)
-
-const initialPersons = [
-    {
-        name: 'John Smith',
-        number: '010-001001'
-    },
-    {
-        name: 'Patrick Smith',
-        number: '020-002002'
-    },
-]
-
-beforeEach(async () => {
-    await Person.deleteMany({})
-    let personObject = new Person(initialPersons[0])
-    await personObject.save()
-    personObject = new Person(initialPersons[1])
-    await personObject.save()
-})
-
-test('persons are returned as json', async () => {
-  await api
-    .get('/api/persons')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('there are two persons', async () => {
-    const response = await api.get('/api/persons')
-  
-    expect(response.body).toHaveLength(initialPersons.length)
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4)
   })
 
-afterAll(() => {
-  mongoose.connection.close()
-})
+  test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+  });
+
+
